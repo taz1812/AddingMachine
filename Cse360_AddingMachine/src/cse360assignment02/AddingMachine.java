875c5084cab8fb2f00fb82bf46704129626dbdb6
alisha <alisha@LAPTOP-G1G3G50Q>
2020-10-04 16:35:37 -0700

    //Initialize private history = 0
    //return cuurent total in getTotal()
    
    //add(int value) should add parameter to total variable
    
    //subtract(int value) should subtract parameter to total variable
    
    //store history of transaction in toString()
    
    //appropriate operation signs

--- Cse360_AddingMachine/src/cse360assignment02/AddingMachine.java
+++ Cse360_AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,40 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
+	  private int total;
+	  //initialize string history to 0 to initialize
+	  private String history = "0";
 
-  public void subtract (int value) {
-  }
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
 
-  public String toString () {
-    return "";
-  }
+	  public int getTotal () {
+		  //The getTotal method should return the current total
+	    return total;
+	  }
 
-  public void clear() {
-  }
-}
+	  public void add (int value) {
+		  //add method should add the parameter to the total variable
+		  //total += value;
+		  total = total + value;
+		  history += " + " + value;
+	  }
+
+	  public void subtract (int value) {
+		  //The subtract method should subtract the parameter from the total variable
+		  //total -= value;
+		  total = total - value;
+		  history += " - " + value;
+	  }
+
+	  public String toString () {
+		  //histroy of transaction must be stored in here and start at 0
+	    return history;
+	  }
+
+	  public void clear() {
+		  //clear memory
+	  }
+
+	}
